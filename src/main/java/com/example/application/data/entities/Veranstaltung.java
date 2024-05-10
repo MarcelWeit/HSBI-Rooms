0d2297c940dda0292f7104d735ec704af7fa7fc2
Marcel Weithöner <marcel.weithoener@outlook.de>
2024-05-10 15:47:32 +0200

    Räume und Ausstattung

--- src/main/java/com/example/application/data/entities/Veranstaltung.java
+++
@@ -1,8 +0,0 @@
-package com.example.application.data.entities;
-
-public class Veranstaltung {
-
-    public Veranstaltung() {
-
-    }
-}
--- src/main/java/com/example/application/data/repository/VeranstaltungRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.application.data.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VeranstaltungRepository {
-}
