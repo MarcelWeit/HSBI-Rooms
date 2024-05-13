27b75ddaab4e7a3e2b4ed77cdb492412075bd011
Leon Gepfner <leongepfner2.0+github@gmail.com>
2024-05-13 23:55:59 +0200

    Add: Entity Klasse.

---
+++ src/main/java/com/example/application/data/entities/Veranstaltung.java
@@ -0,0 +1,33 @@
+package com.example.application.data.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Veranstaltung {
+
+    @Id
+    private String id;
+
+    private String bezeichnung;
+    private String dozent; //Placeholder bis Klasse Dozent
+    private int teilnehmerzahl;
+    private Fachbereich fachbereich;
+
+    public Veranstaltung() {}
+
+    public String getId() {return id;}
+    public String getBezeichnung() {return bezeichnung;}
+    public String getDozent() {return dozent;}
+    public int getTeilnehmerzahl() {return teilnehmerzahl;}
+    public Fachbereich getFachbereich() {return fachbereich;}
+
+    public void setId(String id) {this.id = id;}
+    public void setBezeichnung(String bezeichnung) {this.bezeichnung = bezeichnung;}
+    public void setDozent(String dozent) {this.dozent = dozent;}
+    public void setTeilnehmerzahl(int teilnehmerzahl) {this.teilnehmerzahl = teilnehmerzahl;}
+    public void setFachbereich(Fachbereich fachbereich) {this.fachbereich = fachbereich;}
+
+
+
+}
