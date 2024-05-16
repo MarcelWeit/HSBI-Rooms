05cd3cd8746dba7fa1ba443068bcc3339b4d6022
Leon Gepfner <leongepfner2.0+github@gmail.com>
2024-05-16 10:56:29 +0200

    Add: SQL Logic (still work-in-progress)

--- src/main/java/com/example/application/data/repository/VeranstaltungRepository.java
+++ src/main/java/com/example/application/data/repository/VeranstaltungRepository.java
@@ -6,12 +6,20 @@ import com.example.application.data.entities.Veranstaltung;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
 public interface VeranstaltungRepository extends JpaRepository<Veranstaltung, Long>, JpaSpecificationExecutor<Veranstaltung> {
-    Veranstaltung findById(String id);
-    Set<Veranstaltung> findByBezeichnung(String bezeichnung);
-    Set<Veranstaltung> findByFachbereich(Fachbereich fachbereich);
-    Set<Veranstaltung> findByDozent(String dozent); //Dozent mit Dozent Klasse ersetzen!!!
+//    @Query("select v from Veranstaltung as v where :id in v.id")
+    Veranstaltung findById(@Param("id") String id);
+
+//    @Query("select v from Veranstaltung as v where v.bezeichnung like %:bezeichnung%")
+    Set<Veranstaltung> findByBezeichnung(@Param("bezeichnung") String bezeichnung);
+
+//    @Query("select v from Veranstaltung as v where 'test' like %:fachbereich%")
+    Set<Veranstaltung> findByFachbereich(@Param("fachbereich") Fachbereich fachbereich);
+
+//    @Query("select v from Veranstaltung as v where v.dozent like %:dozent%")
+    Set<Veranstaltung> findByDozent(@Param("dozent") String dozent); //Dozent mit Dozent Klasse ersetzen!!!
 }
