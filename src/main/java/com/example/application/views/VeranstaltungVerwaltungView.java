bae5900afeffafd0191a88384ff396182e4a9e42
Leon Gepfner <leongepfner2.0+github@gmail.com>
2024-05-16 10:56:43 +0200

    Add: Constructor

--- src/main/java/com/example/application/views/VeranstaltungVerwaltungView.java
+++ src/main/java/com/example/application/views/VeranstaltungVerwaltungView.java
@@ -1,10 +1,17 @@
 package com.example.application.views;
 
+import com.example.application.services.VeranstaltungService;
 import com.vaadin.flow.router.PageTitle;
 import jakarta.annotation.security.RolesAllowed;
 
 @PageTitle("Veranstaltungen verwalten")
 @RolesAllowed({"ADMIN", "FBPlanung"})
 public class VeranstaltungVerwaltungView {
+
+    private final VeranstaltungService veranstaltungService;
+
+    public VeranstaltungVerwaltungView(VeranstaltungService veranstaltungService) {
+        this.veranstaltungService = veranstaltungService;
+    }
 
 }
