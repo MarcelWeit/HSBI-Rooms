e11dec51052cb00ed8b30e88e140d0d94174fdbc
Leon Gepfner <leongepfner2.0+github@gmail.com>
2024-05-17 16:29:04 +0200

    Add: View layout

--- src/main/java/com/example/application/views/VeranstaltungVerwaltungView.java
+++ src/main/java/com/example/application/views/VeranstaltungVerwaltungView.java
@@ -1,9 +1,14 @@
 package com.example.application.views;
 
+import com.example.application.data.entities.Veranstaltung;
 import com.example.application.services.VeranstaltungService;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.crud.Crud;
+import com.vaadin.flow.component.crud.CrudEditor;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
@@ -11,13 +16,31 @@ import jakarta.annotation.security.RolesAllowed;
 @Route(value = "veranstaltungVerwaltung-crud", layout = MainLayout.class)
 @RolesAllowed({"ADMIN", "FBPlanung"})
 @Uses(Icon.class)
-@PageTitle("VeranstaltungsVerwaltung")
+@PageTitle("Veranstaltungsen")
 public class VeranstaltungVerwaltungView extends VerticalLayout {
 
     private final VeranstaltungService veranstaltungService;
 
+    private Crud crud;
+
     public VeranstaltungVerwaltungView(VeranstaltungService veranstaltungService) {
         this.veranstaltungService = veranstaltungService;
+
+        crud = new Crud<>(Veranstaltung.class, createEditor());
+
+        this.createComponents();
+    }
+
+    private void createComponents() {
+
+    }
+    private CrudEditor<Veranstaltung> createEditor() {
+
+        TextField veranstaltung = new TextField("VeranstaltungsID");
+        TextField bezeichnung = new TextField("Bezeichnung");
+
+        ComboBox dozent = new ComboBox("Dozent");
+
     }
 
 }
