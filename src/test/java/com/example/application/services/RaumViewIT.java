fdac87585f565996d9aa08e171614dd2ef045a56
Marcel Weithoener <marcel.weithoener@outlook.de>
2024-06-07 17:03:33 +0200

    selenium tests

--- src/test/java/com/example/application/services/RaumViewIT.java
+++ src/test/java/com/example/application/services/RaumViewIT.java
@@ -41,8 +41,41 @@ public class RaumViewIT {
                 .until(d -> d.getTitle().startsWith("Räume verwalten"));
 
         var addRoomButton = driver.findElement(By.xpath("//vaadin-button[contains(.,'Raum hinzufügen')]"));
+        var editRoomButton = driver.findElement(By.xpath("//vaadin-button[contains(.,'Raum bearbeiten')]"));
+        var deleteRoomButton = driver.findElement(By.xpath("//vaadin-button[contains(.,'Raum löschen')]"));
+        var bookRoomButton = driver.findElement(By.xpath("//vaadin-button[contains(.,'Raum buchen')]"));
+        var showBookingsButton = driver.findElement(By.xpath("//vaadin-button[contains(.,'Buchungen anzeigen')]"));
+        var kwButton = driver.findElement(By.xpath("//vaadin-button[contains(.,'KW Verfügbarkeit')]"));
 
         assertEquals("Raum hinzufügen", addRoomButton.getText());
+        assertEquals("Raum bearbeiten", editRoomButton.getText());
+        assertEquals("Raum löschen", deleteRoomButton.getText());
+        assertEquals("Raum buchen", bookRoomButton.getText());
+        assertEquals("Buchungen anzeigen", showBookingsButton.getText());
+        assertEquals("KW Verfügbarkeit", kwButton.getText());
+    }
+
+    @Test
+    public void addRoom(){
+        driver.get("http://localhost:8080/raumverwaltung");
+        new WebDriverWait(driver, Duration.ofSeconds(30), Duration.ofSeconds(1))
+                .until(d -> d.getTitle().startsWith("Räume verwalten"));
+
+        var addRoomButton = driver.findElement(By.xpath("//vaadin-button[contains(.,'Raum hinzufügen')]"));
+        addRoomButton.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(30), Duration.ofSeconds(1))
+                .until(d -> d.findElement(By.xpath("//vaadin-button[contains(.,'Speichern')]")));
+
+        driver.findElement(By.id("input-vaadin-text-field-47")).sendKeys("C337");
+        driver.findElement(By.id("input-vaadin-combo-box-49")).sendKeys("Wirtschaft");
+        driver.findElement(By.id("input-vaadin-text-field-50")).sendKeys("Fachbereich Wirtschaft Etage 1");
+        driver.findElement(By.id("input-vaadin-combo-box-52")).sendKeys("Hörsaal");
+//        driver.findElement(By.id("input-vaadin-integer-field-53")).sendKeys("100");
+        driver.findElement(By.id("input-vaadin-multi-select-combo-box-55")).sendKeys("Beamer");
+
+        driver.findElement(By.xpath("//vaadin-button[contains(.,'Speichern')]")).click();
+
     }
 
     @AfterEach
